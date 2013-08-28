a8de1482a3630ff40097e61d215bf9fcdf9e4c58
liobouchan <leo.bouchan@gmail.com>
2013-08-28 00:06:23 -0500

    Programa 13,14,15

---
+++ 13.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	printf("%s%c","Hola a \
+			todos",'\n');
+
+	return 0;			
+}
---
+++ 14.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	printf("Hola\
+			a \
+			todos\n");
+	return 0;		
+}
