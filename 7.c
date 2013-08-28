399edb95755797a68fb7f0be489f7fac961055d6
liobouchan <leo.bouchan@gmail.com>
2013-08-28 00:04:16 -0500

    Programa 7,8,9

---
+++ 7.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	printf("%s %s\n","Hola a",
+			"todos");
+	return 0;		
+}
---
+++ 8.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	char* mensaje= "Hola a todos";
+	printf("%s\n", mensaje);
+	return 0;
+}
