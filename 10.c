7f7ac3a421408dc099ff62ee380d7330040ab171
liobouchan <leo.bouchan@gmail.com>
2013-08-28 00:05:21 -0500

    Programa 10,11,12

---
+++ 10.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	char* mensaje="Hola a todos\n";
+	printf(mensaje);
+	return 0;
+}
---
+++ 11.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	char* mensaje = "Hola a %s";
+	printf(mensaje,"todos\n");
+	return 0;
+}
