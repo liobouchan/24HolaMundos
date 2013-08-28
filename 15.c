5d294e9a3a104a8612cd7627ddf3c210af3e209c
liobouchan <leo.bouchan@gmail.com>
2013-08-28 00:06:59 -0500

    Programa 16

---
+++ 15.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	printf("Hola a todos",
+			"Nada más");
+	return 0;		
+}
---
+++ 16.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	printf("\x48\x6F\x6C\x61\x20\x61\
+			\x20\x74\x6F\x64\x6F\x73\n");
+	return 0;		
+}
